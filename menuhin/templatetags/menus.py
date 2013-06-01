30edd6ae6988ffb286b8be8c1975d596b0a1c232
Keryn Knight <keryn@kerynknight.com>
2013-06-01 16:48:39 +0100

    implementing "show_sub_menu" - allowing for giving an object OR url to get the descendants to N depth

--- menuhin/templatetags/menus.py
+++ menuhin/templatetags/menus.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from copy import copy
-from classytags.core import Tag, Options
+import logging
+from classytags.core import Options
 from classytags.arguments import Argument
 from classytags.helpers import InclusionTag, AsTag
 from django import template
@@ -8,9 +8,10 @@ import itertools
 from django.core.validators import validate_slug
 from menuhin.utils import get_menu, get_all_menus
 
+
 register = template.Library()
 
-
+logger = logging.getLogger(__name__)
 
 class ShowMenu(InclusionTag):
     template = 'menuhin/none.html'
@@ -21,10 +22,6 @@ class ShowMenu(InclusionTag):
     )
 
     def get_context(self, context, title, from_depth, to_depth, **kwargs):
-        # make sure we haven't been dumb as hammers by validating this title
-        # is something we could actually be storing.
-        validate_slug(title)
-
         request = None
         if 'request' in context:
             request = context['request']
@@ -41,6 +38,7 @@ class ShowMenu(InclusionTag):
             'menu': menu,
             'from_depth': from_depth,
             'to_depth': to_depth,
+            'is_submenu': False,
         }
 
     def get_template(self, context, title, **kwargs):
@@ -52,32 +50,68 @@ class ShowMenu(InclusionTag):
 register.tag(name='show_menu', compile_function=ShowMenu)
 
 
-class ShowAllMenus(InclusionTag):
-    template = 'menuhin/none.html'
-#    options = Options(
-#        Argument('title', required=False, resolve=True, default=None),
-#    )
+class GetMenu(AsTag):
+    options = ShowMenu.options
 
-    def get_context(self, context):
-        items = get_all_menus()
-        import pdb; pdb.set_trace()
-        return {}
+    def get_value(self, *args, **kwargs):
+        return ShowMenu().get_context(*args, **kwargs)
+register.tag(name='get_menu', compile_function=GetMenu)
 
-    def get_template(self, context):
+
+class ShowSubMenu(InclusionTag):
+    template = ShowMenu.template
+    options = Options(
+        Argument('url', required=True, resolve=True, default=100),
+        Argument('to_depth', required=False, resolve=True, default=100),
+        Argument('menu', required=False, resolve=True, default=None),
+    )
+
+    def get_context(self, context, url, to_depth, menu, **kwargs):
+        # we can look for the request now that we know we can't return a result
+        # from the cache; as we're passing the request back to the menu backend
+        # we need it ...
+        if 'request' not in context:
+            logger.warning('request not in Context')
+            return {}
+        request = context['request']
+
+        if menu:
+            logger.debug('finding matching node for for %s only' % menu)
+            items = get_menu(menu, request=request).nodes
+        else:
+            logger.debug('finding matching node using all menus')
+            items = get_all_menus(request=request)
+            items = itertools.chain.from_iterable([x.nodes for x in items.values()])
+
+        try:
+            url = url.get_absolute_url()
+        except AttributeError:
+            # Might've been an object with a get_absolute_url method. Wasn't.
+            # Now we're assuming it's a string or something we can actually use.
+            pass
+
+        # hmmm, this could probably use the active calculator?
+        try:
+            first_active_node = (x for x in items if x.url == url).next()
+            max_depth = first_active_node.depth + to_depth
+            descendants = (x for x in first_active_node.descendants
+                           if x.depth <= max_depth)
+        except StopIteration as e:
+            first_active_node = None
+            descendants = ()
+
+        return {
+            'node': first_active_node,
+            'nodes': descendants,
+            'menu': menu,
+            'to_depth': to_depth,
+            'is_submenu': True,
+        }
+
+    def get_template(self, context, menu, **kwargs):
         return [
-            'menuhin/show_all_menus/default.html',
+            'menuhin/show_menu/%s/default.html' % menu,
+            'menuhin/show_menu/default.html',
             self.template,
         ]
-register.tag(name='show_all_menus', compile_function=ShowAllMenus)
-
-class GetMenu(AsTag):
-    options = Options(
-        Argument('title', required=False, resolve=True, default=None),
-    )
-    def get_value(self, context, title):
-        menu = get_menu(title)
-        return {
-            'nodes': menu.nodes,
-            'menu': menu,
-        }
-register.tag(name='get_menu', compile_function=GetMenu)
+register.tag(name='show_sub_menu', compile_function=ShowSubMenu)
