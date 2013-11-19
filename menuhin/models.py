d3722f4c3892d79e32e2b15cedd0a86cd2e8ca6e
Keryn Knight <keryn@kerynknight.com>
2013-11-19 15:12:22 +0000

    changing how everything works.

--- menuhin/models.py
+++ menuhin/models.py
@@ -1,27 +1,20 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 import logging
 from urlparse import urlparse
 from django.conf import settings
 from django.contrib.sites.models import Site
-from django.core.cache import cache
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import SlugField, ForeignKey, CharField
+from django.contrib.contenttypes.models import ContentType
 from django.http import QueryDict
 from django.utils.importlib import import_module
 from model_utils import Choices
 from model_utils.managers import PassThroughManager
 from helpfulfields.models import ChangeTracking, Publishing
-from menuhin.settings import MENUHIN_MENU_HANDLERS, NODE_CACHE_KEY_PREFIX, TREE_CACHE_KEY_PREFIX
+from menuhin.settings import MENUHIN_MENU_HANDLERS
 from menuhin.text import (menu_v, menu_vp, title_label, title_help,
                           display_title_label, display_title_help)
-from menuhin.querysets import (MenuQuerySet, CustomMenuLabelQuerySet,
-                               CustomMenuItemQuerySet)
-
-# see the following URLs:
-# http://bobobobo.wordpress.com/2010/12/22/closure-table-part-deux-nodes-and-adjacencies-a-tree-in-mysql/
-# http://dirtsimple.org/2010/11/simplest-way-to-do-tree-based-queries.html
-# https://gist.github.com/1380975
+from menuhin.querysets import MenuQuerySet, CustomMenuItemQuerySet, MenuFinder
 
 
 logger = logging.getLogger(__name__)
@@ -35,36 +28,36 @@ class Menu(ChangeTracking, Publishing):
                               help_text=display_title_help)
 
     objects = PassThroughManager.for_queryset_class(MenuQuerySet)()
+    menus = MenuFinder()
 
     def __unicode__(self):
         """Reserved for admin/form usage."""
         return self.title
 
+    def get_nodes(self, parent_node=None):
+        raise NotImplementedError('Subclasses should provide this implementation')
+
     class Meta:
         verbose_name = menu_v
         verbose_name_plural = menu_vp
-
-
-class CustomMenuLabel(ChangeTracking, Publishing):
-    menu = ForeignKey(Menu)
-    title = CharField(max_length=100, verbose_name=display_title_label)
-    target_id = CharField(max_length=100)
-    objects = PassThroughManager.for_queryset_class(CustomMenuLabelQuerySet)()
+        unique_together = ('site', 'title')
 
 
 class CustomMenuItem(ChangeTracking, Publishing):
-    menu = ForeignKey(Menu)
-    POSITIONS = Choices('above', 'below')
+    menu = ForeignKey(Menu, related_name="menu_items")
+    POSITIONS = Choices('above', 'replacing', 'below')
     position = CharField(choices=POSITIONS, default=POSITIONS.above, max_length=5)
     title = CharField(max_length=100, verbose_name=display_title_label)
     url = CharField(max_length=2048)
+    # store the parent_id of the menunode to work with.
     parent_id = CharField(max_length=100)
+    attach_menu = ForeignKey(Menu, related_name="attached_to", null=True)
     objects = PassThroughManager.for_queryset_class(CustomMenuItemQuerySet)()
 
 
 class MenuNode(object):
-    __slots__ = ('title', 'url', 'unique_id', 'parent_id',
-                 'depth', 'ancestors', 'descendants', 'extra_context')
+    __slots__ = ('title', 'url', 'unique_id', 'parent_id', 'depth', 'ancestors',
+                 'descendants', 'extra_context', '_object', 'object')
 
     def __init__(self, title, url, unique_id, parent_id=None, **kwargs):
         self.title = title
@@ -77,20 +70,47 @@ class MenuNode(object):
         self.ancestors = []
         self.descendants = []
         self.extra_context = kwargs or {}
+        self._object = None
 
     def get_absolute_url(self):
         """Familiar API ..."""
         return self.url
 
+    def _get_object(self):
+        if self._object is None:
+            if 'content_type' not in self.extra_context:
+                logger.warning('Attempted to access `MenuNode.object` without'
+                               'providing `content_type`')
+                return self._object
+            if 'object_id' not in self.extra_context:
+                logger.warning('Attempted to access `MenuNode.object` without'
+                               'providing `object_id`')
+                return self._object
+
+            model = ContentType.objects.get_for_id(
+                self.extra_context['content_type'])
+            try:
+                self._object = model.model_class()._default_manager.get(
+                    pk=self.extra_context['object_id'])
+            except ObjectDoesNotExist as e:
+                return self._object
+
+        return self._object
+
+    def _set_object(self, obj):
+        """
+        Forcibly set the object.
+        """
+        self._object = obj
+
+    object = property(_get_object, _set_object)
+
     def __unicode__(self):
         return self.title
 
     def __repr__(self):
-        data = {'id': self.unique_id, 'parent': self.parent_id,
-                'title': self.title, 'level': self.depth}
-        repr = ('<menuhin.models.MenuNode: %(level)d (%(id)s, %(parent)s, '
-                '"%(title)s")>')
-        return repr % data
+        return ('<menuhin.models.MenuNode: {obj.depth} ({obj.unique_id}, '
+                '{obj.parent_id}, {obj.title})>').format(obj=self)
 
     def __nonzero__(self):
         """
@@ -106,6 +126,34 @@ class MenuNode(object):
     def __ne__(self, other):
         return other.unique_id != self.unique_id
 
+    # def __getattr__(self, attr):
+    #     """
+    #     Anything not in `__slots__` should be pulled out of the extra context
+    #     dictionary.
+    #
+    #     :param attr: attribute name to look up.
+    #     :return: the value stored in the `extra_context` dictionary.
+    #     """
+    #     return self.extra_context.get(attr, None)
+    #
+    # __getitem__ = __getattr__
+    #
+    # def __setattr__(self, key, value):
+    #     """
+    #     If the `key` is one which we know about (ie: is in `__slots__`) then
+    #     apply it directly, otherwise push it into the `extra_context`
+    #
+    #     :param key: the attribute to try and set.
+    #     :param value: the value to apply to either the object or the dictionary.
+    #     :return: :data:`None`
+    #     """
+    #     if hasattr(self, key):
+    #         object.__setattr__(self, key, value)
+    #     else:
+    #         self.extra_context.update(key=value)
+    #
+    # __setitem__ = __setattr__
+
     def __contains__(self, item):
         """
         Allows for doing "Is Node A in Node B?" without being clear about
@@ -116,10 +164,11 @@ class MenuNode(object):
         """
         return item in self.ancestors or item in self.descendants
 
+
 class AncestryCalculator(object):
     __slots__ = ()
 
-    def __call__(self, this_node, other_nodes, **kwargs):
+    def __call__(self, this_node, other_nodes, request, **kwargs):
         ancestors = []
         parent = this_node.unique_id
         while parent is not None:
@@ -134,7 +183,7 @@ class AncestryCalculator(object):
 class DescendantCalculator(object):
     __slots__ = ()
 
-    def __call__(self, this_node, other_nodes, **kwargs):
+    def __call__(self, this_node, other_nodes, request, **kwargs):
         # we should already have ancestors as a sorted list, so we can just
         # apply descendants in order?
         ancestors = []
@@ -149,10 +198,36 @@ class DepthCalculator(object):
     def __init__(self, start=0):
         self.start = start
 
-    def __call__(self, this_node, other_nodes, **kwargs):
+    def __call__(self, this_node, other_nodes, request, **kwargs):
         this_node.depth = len(this_node.ancestors) + self.start
         return this_node
 
+
+class HeirarchyCalculator(object):
+    __slots__ = ('start',)
+
+    def __init__(self, start=0):
+        self.start = start
+
+    def __call__(self, this_node, other_nodes, request, **kwargs):
+        ancestors = []
+        parent = this_node.unique_id
+        # import pdb; pdb.set_trace()
+        while parent is not None:
+            if parent not in other_nodes:
+                parent = None
+            else:
+                next_parent = other_nodes[parent]
+                ancestors.append(next_parent)
+                parent = next_parent.parent_id
+        this_node.ancestors = ancestors[::-1]
+
+        # import pdb; pdb.set_trace()
+
+        for node in this_node.ancestors:
+            node.descendants.append(this_node)
+
+        return this_node
 
 class ActiveCalculator(object):
     __slots__ = ('compare_querystrings',)
@@ -160,7 +235,7 @@ class ActiveCalculator(object):
     def __init__(self, compare_querystrings=True):
         self.compare_querystrings = True
 
-    def __call__(self, this_node, request, **kwargs):
+    def __call__(self, this_node, other_nodes, request, **kwargs):
         try:
             req_url = request.get_full_path()
         except AttributeError as e:
@@ -237,12 +312,13 @@ class MenuCollection(object):
         """
         name = self.get_name()
         try:
-            logger.debug('Getting menu from database')
+            logger.debug('Getting menu `{0}` from database'.format(name))
             menu = Menu.objects.get(title=name)
         except Menu.DoesNotExist:
-            logger.info('Menu not found in database, creating "%s" now' % name)
-            menu = Menu(title=name,
-                        display_title=self.get_verbose_name())
+            title = self.get_verbose_name()
+            logger.info('Menu `{name}` not found in database, creating '
+                        '"{title}" now'.format(name=name, title=title))
+            menu = Menu(title=name, display_title=title)
             menu.full_clean()
             menu.save()
         return menu
--- menuhin/querysets.py
+++ menuhin/querysets.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.db.models import OneToOneField
+from django.template.defaultfilters import slugify
 from helpfulfields.querysets import ChangeTrackingQuerySet, PublishingQuerySet
 
 
@@ -6,9 +8,131 @@ class MenuQuerySet(ChangeTrackingQuerySet, PublishingQuerySet):
     pass
 
 
-class CustomMenuLabelQuerySet(ChangeTrackingQuerySet, PublishingQuerySet):
-    pass
-
-
 class CustomMenuItemQuerySet(ChangeTrackingQuerySet, PublishingQuerySet):
     pass
+
+
+class MenuFinder(object):
+    def __get__(self, instance, owner):
+        self.model = owner
+        return self
+
+    def _all_models_oldstyle(self, model):
+        """
+        Discovery of menus via unmanaged models
+        """
+        objs = [rel for rel in model._meta.get_all_related_objects()
+                if isinstance(rel.field, OneToOneField)
+                and issubclass(rel.field.model, model)
+                and rel.field.model._meta.managed is False]
+        for obj in objs:
+            yield obj
+            for subobj in self._all_models(model=obj.field.model):
+                yield subobj
+
+    def _all_models(self, model):
+        """
+        Discovery of menus via proxy models
+        """
+        objs = [rel for rel in model.__subclasses__()
+                if hasattr(rel, '_meta') and rel._meta.proxy is True]
+        for obj in objs:
+            yield obj
+            for subobj in self._all_models(model=obj):
+                yield subobj
+
+    def models(self):
+        """
+        Returns all possible menu model classes (not instances)
+        """
+        for model in self._all_models(model=self.model):
+            yield model
+
+    def get_or_create(self, site=None):
+        """
+        Creates all of the discovered menus ...
+        """
+        if site is None:
+            site = self.model._meta.get_field_by_name('site')[0].default()
+        for model in self.models():
+            arguments = {
+                'title': slugify(model._meta.verbose_name)[:50],
+                'display_title': model._meta.verbose_name[:50],
+                'site': site,
+            }
+            fixed_arguments = dict((k, v) for k, v in arguments.iteritems()
+                                   if v)
+            yield self.model.objects.get_or_create(**fixed_arguments)
+
+
+    def get_nodes(self, parent_node=None):
+        return self.model().get_nodes(parent_node=parent_node)
+
+    def get_processed_nodes(self, request, parent_node=None):
+        nodes = self.get_nodes(parent_node=parent_node)
+        if not self.model.processors:
+            yield nodes
+
+        # ugh :(
+        tree = {}
+        nodes = list(nodes)
+        for element in nodes:
+            tree[element.unique_id] = element
+
+        for node in nodes:
+            for processor in self.model.processors:
+                yield processor(this_node=node, other_nodes=tree,
+                                request=request)
+    #
+    # def build_tree(self):
+    #     """
+    #     internal nodes representation!
+    #     basically:
+    #     {
+    #       'unique_id': node,
+    #       'unique_id': node,
+    #     }
+    #     """
+    #     assert self._meta.managed is False, "Stop trying to call this on " \
+    #                                         "managed models"
+    #     self.tree = {}
+    #     for element in self.nodes:
+    #         self.tree[element.unique_id] = element
+    #     return self
+    #
+    # def build_nodes(self):
+    #     self.nodes = list(self.get_nodes())
+    #     return self
+    #
+    # def apply_safe_processors(self):
+    #     # process nodes
+    #     # This might be expensive, so use sparingly.
+    #     if self.processors:
+    #         for node in self.nodes:
+    #             for processor in self.processors:
+    #                 node = processor(this_node=node,
+    #                                  other_nodes=self.tree)
+    #     return self
+    #
+    # def calculate_tree(self):
+    #     self.tree = {}
+    #     for element in self.nodes:
+    #         self.tree[element.unique_id] = element
+    #     return self
+    #
+    # def build(self, request=None):
+    #     self.build_nodes().build_tree().apply_safe_processors()
+    #     return self
+    #
+    # def all(self, request):
+    #     is_active = ActiveCalculator(compare_querystrings=True)
+    #     return (is_active(this_node=x, request=request) for x in self.nodes)
+    #
+    # def filter_depth(self, request, from_depth, to_depth):
+    #     return (x for x in self.all(request)
+    #             if x.depth >= from_depth and x.depth <=to_depth)
+    #
+    # def filter_active(self, request):
+    #     return (x for x in self.all(request)
+    #             if 'is_active' in x.extra_context
+    #             and x.extra_context['is_active'])
