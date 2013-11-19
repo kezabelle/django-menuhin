49356b962bf91d3477f1302d902e43e09ee8360b
Keryn Knight <keryn@kerynknight.com>
2013-11-19 15:11:41 +0000

    go unicode literals!

--- menuhin/text.py
+++ menuhin/text.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 
-title_label = _(u'unique title')
-title_help = _(u'an internally consistent title used to find menus. ')
+title_label = _('unique title')
+title_help = _('an internally consistent title used to find menus. ')
 
-display_title_label = _(u'title')
-display_title_help = _(u'a meaningful description of this menu')
+display_title_label = _('title')
+display_title_help = _('a meaningful description of this men')
 
-config_fieldset_label = _(u'menu configuration')
+config_fieldset_label = _('menu configuration')
 
-menu_v = _(u'menu')
-menu_vp = _(u'menus')
-menuitem_v = _(u'Menu item')
-menuitem_vp = _(u'Menu items')
+menu_v = _('menu')
+menu_vp = _('menus')
+menuitem_v = _('Menu item')
+menuitem_vp = _('Menu items')
