f471e1099108f69173ffd5c4cf9606aec3cd4cba
Keryn Knight <keryn@kerynknight.com>
2013-11-20 13:10:34 +0000

    fix texts

--- menuhin/text.py
+++ menuhin/text.py
@@ -3,14 +3,14 @@ from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 
 title_label = _('unique title')
-title_help = _('an internally consistent title used to find menus. ')
+title_help = _('an internally consistent title used to find menus.')
 
 display_title_label = _('title')
-display_title_help = _('a meaningful description of this men')
+display_title_help = _('a meaningful description of this menu')
 
 config_fieldset_label = _('menu configuration')
 
 menu_v = _('menu')
 menu_vp = _('menus')
-menuitem_v = _('Menu item')
-menuitem_vp = _('Menu items')
+menuitem_v = _('menu item')
+menuitem_vp = _('menu items')
